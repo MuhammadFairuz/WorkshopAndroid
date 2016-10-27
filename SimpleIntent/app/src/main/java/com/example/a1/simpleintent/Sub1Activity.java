ce0343c2f139b0445766a370c967ac190f64b1bd
MuhammadFairuz <fairuzzumar@gmail.com>
2016-10-27 12:43:14 +0700

    firstCommit

---
+++ SimpleIntent/app/src/main/java/com/example/a1/simpleintent/Sub1Activity.java
@@ -0,0 +1,14 @@
+package com.example.a1.simpleintent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Sub1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub1);
+
+    }
+}
